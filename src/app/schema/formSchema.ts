591c2c42e9aa00b9ec5d12454c2d9fce0c82be0a
sayyam.techtimize <sayyam.techtimize@gmail.com>
2025-03-11 14:19:35 +0500

    update validation message

--- src/app/schema/formSchema.ts
+++ src/app/schema/formSchema.ts
@@ -5,11 +5,11 @@ const phoneRegex = new RegExp(
   );
   
 export const formSchema= z.object({
-    username: z.string().min(3),
+    username: z.string().min(3,{message:"Username must be 3 character long"}),
     email: z.string().email({message:"Email must be a valid email address"}),
     phone: z.string().regex(phoneRegex, 'Invalid Number!'),
-    message: z.string().min(3),
-    options:z.string()
+    message: z.string().min(3,{message:"Message should be longer"}),
+    options:z.string().min(1,{message:"Select atleast one option"})
 })
 
 export type FormValidator = z.infer<typeof formSchema>;
