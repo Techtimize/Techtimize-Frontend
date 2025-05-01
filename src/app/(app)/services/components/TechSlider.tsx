67fc19a93697ca0dbeb0f52267520ee62e1e23fd
shahriyarkashif <shahriyar.techtimize@gmail.com>
2025-05-01 17:00:04 +0500

    feat(ui): scroll services section into view when navigating to the respective service

--- src/app/(app)/services/components/TechSlider.tsx
+++ src/app/(app)/services/components/TechSlider.tsx
@@ -1,27 +1,41 @@
 "use client";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Image from "next/image";
 import useEmblaCarousel from "embla-carousel-react";
 import AutoScroll from "embla-carousel-auto-scroll";
 import stackProps from "@/app/types/stacks.type";
+import { useSearchParams } from "next/navigation";
 
-export default function TechSlider({ servicedata }: { servicedata: stackProps[] }) {
-  const [emblaRef] = useEmblaCarousel(
-    { loop: true, align: "start" },
-    [
-      AutoScroll({
-        speed: 4, // Increase speed to make it noticeable
-        stopOnInteraction: false,
-      }),
-    ]
-  );
+export default function TechSlider({
+  servicedata,
+}: {
+  servicedata: stackProps[];
+}) {
+  const ref = useRef<HTMLDivElement>(null);
+  const query = useSearchParams();
+  const [emblaRef] = useEmblaCarousel({ loop: true, align: "start" }, [
+    AutoScroll({
+      speed: 4, // Increase speed to make it noticeable
+      stopOnInteraction: false,
+    }),
+  ]);
+
+  useEffect(() => {
+    if (ref.current && query.get("service")) {
+      ref.current.scrollIntoView();
+    }
+  }, [query]);
 
   return (
     <div className="overflow-hidden xl:py-[40px] md:py-[30px] py-[20px]">
       <div className="embla" ref={emblaRef}>
         <div className="embla__container flex">
           {servicedata.map((service) => (
-            <div key={service._id} className="embla__slide flex-none w-auto">
+            <div
+              key={service._id}
+              className="embla__slide flex-none w-auto"
+              ref={ref}
+            >
               <div className="flex items-center justify-center h-full">
                 <Image
                   src={service.serviceImage}
--- src/app/(app)/services/http/fetchstack.tsx
+++ src/app/(app)/services/http/fetchstack.tsx
@@ -1,7 +1,12 @@
+import { Suspense } from "react";
 import TechSlider from "../components/TechSlider";
 import GetStacks from "@/app/api/stacks/get_stacks";
 
-export default async function GetAllStacks(){
-    const servicedata = await GetStacks();
-    return <TechSlider servicedata={servicedata}/>;
+export default async function GetAllStacks() {
+  const servicedata = await GetStacks();
+  return (
+    <Suspense fallback={<div>Loading</div>}>
+      <TechSlider servicedata={servicedata} />;
+    </Suspense>
+  );
 }
